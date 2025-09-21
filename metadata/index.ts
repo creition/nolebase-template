f2c83097d7ab1020aee177d3a82e41c4d301b4a0
creition <abc13839467626@outlook.com>
2025-09-21 10:56:41 +0800

    更新 index.ts

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = '咕子'
+export const siteShortName = '咕子子'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
